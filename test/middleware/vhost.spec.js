a6b59c1887053acc17589e1da2ce744ee457b413
Kyle Herock <kherock@gmail.com>
2020-06-07 16:55:34 -0400

    treat OS hostname as S3 service root

--- test/middleware/vhost.spec.js
+++ test/middleware/vhost.spec.js
@@ -5,6 +5,7 @@ const xmlParser = require('fast-xml-parser');
 const { zip } = require('lodash');
 const he = require('he');
 const moment = require('moment');
+const os = require('os');
 const request = require('request-promise-native').defaults({
   resolveWithFullResponse: true,
 });
@@ -42,6 +43,26 @@ describe('Virtual Host resolution', () => {
     });
     const res = await request(s3Client.config.endpoint, {
       headers: { host: 's3.example.com' },
+    });
+    const parsedBody = xmlParser.parse(res.body, {
+      tagValueProcessor: a => he.decode(a),
+    });
+    expect(parsedBody).to.haveOwnProperty('ListAllMyBucketsResult');
+    const parsedBuckets = parsedBody.ListAllMyBucketsResult.Buckets.Bucket;
+    expect(parsedBuckets).to.be.instanceOf(Array);
+    expect(parsedBuckets).to.have.lengthOf(buckets.length);
+    for (const [bucket, config] of zip(parsedBuckets, buckets)) {
+      expect(bucket.Name).to.equal(config.name);
+      expect(moment(bucket.CreationDate).isValid()).to.be.true;
+    }
+  });
+
+  it('lists buckets at the OS hostname', async function() {
+    const { s3Client } = await createServerAndClient({
+      configureBuckets: buckets,
+    });
+    const res = await request(s3Client.config.endpoint, {
+      headers: { host: os.hostname() },
     });
     const parsedBody = xmlParser.parse(res.body, {
       tagValueProcessor: a => he.decode(a),
