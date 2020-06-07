89d85fcf549301d2155b99a312123a9be20a22ff
Kyle Herock <kherock@gmail.com>
2020-06-07 17:18:34 -0400

    support disabling vhost-style bucket access

--- test/middleware/vhost.spec.js
+++ test/middleware/vhost.spec.js
@@ -13,27 +13,47 @@ const request = require('request-promise-native').defaults({
 const { createServerAndClient } = require('../helpers');
 
 describe('Virtual Host resolution', () => {
-  let s3Client;
-  const buckets = [{ name: 'bucket0' }, { name: 'bucket1' }];
+  const buckets = [{ name: 'bucket-a' }, { name: 'bucket-b' }];
 
-  beforeEach(async function() {
-    ({ s3Client } = await createServerAndClient({
+  it('lists objects with subdomain-domain style bucket access', async function() {
+    const { s3Client } = await createServerAndClient({
       configureBuckets: buckets,
-    }));
+    });
+    const res = await request(s3Client.config.endpoint, {
+      headers: { host: 'bucket-a.s3.amazonaws.com' },
+    });
+    expect(res.body).to.include(`<Name>bucket-a</Name>`);
   });
 
-  it('reaches the server with a bucket subdomain', async function() {
-    const res = await request(s3Client.config.endpoint, {
-      headers: { host: 'bucket0.s3.amazonaws.com' },
+  it('lists objects with a vhost-style bucket access', async function() {
+    const { s3Client } = await createServerAndClient({
+      configureBuckets: buckets,
     });
-    expect(res.body).to.include(`<Name>bucket0</Name>`);
+    const res = await request(s3Client.config.endpoint, {
+      headers: { host: 'bucket-a' },
+    });
+    expect(res.body).to.include(`<Name>bucket-a</Name>`);
   });
 
-  it('reaches the server with a bucket vhost', async function() {
-    const res = await request(s3Client.config.endpoint, {
-      headers: { host: 'bucket0' },
+  it('lists buckets when vhost-style bucket access is disabled', async function() {
+    const { s3Client } = await createServerAndClient({
+      vhostBuckets: false,
+      configureBuckets: buckets,
     });
-    expect(res.body).to.include(`<Name>bucket0</Name>`);
+    const res = await request(s3Client.config.endpoint, {
+      headers: { host: 'bucket-a' },
+    });
+    const parsedBody = xmlParser.parse(res.body, {
+      tagValueProcessor: a => he.decode(a),
+    });
+    expect(parsedBody).to.haveOwnProperty('ListAllMyBucketsResult');
+    const parsedBuckets = parsedBody.ListAllMyBucketsResult.Buckets.Bucket;
+    expect(parsedBuckets).to.be.instanceOf(Array);
+    expect(parsedBuckets).to.have.lengthOf(buckets.length);
+    for (const [bucket, config] of zip(parsedBuckets, buckets)) {
+      expect(bucket.Name).to.equal(config.name);
+      expect(moment(bucket.CreationDate).isValid()).to.be.true;
+    }
   });
 
   it('lists buckets at a custom service endpoint', async function() {
@@ -83,11 +103,11 @@ describe('Virtual Host resolution', () => {
       configureBuckets: buckets,
     });
     const res = await request(s3Client.config.endpoint, {
-      headers: { host: 'bucket0.s3.example.com' },
+      headers: { host: 'bucket-a.s3.example.com' },
     });
     const parsedBody = xmlParser.parse(res.body, {
       tagValueProcessor: a => he.decode(a),
     });
-    expect(parsedBody.ListBucketResult.Name).to.equal('bucket0');
+    expect(parsedBody.ListBucketResult.Name).to.equal('bucket-a');
   });
 });
